edd76f122e0dc18d2bd8fc8f7e2d9b90459f1cc9
Woofka <v.d.sobolev@ya.ru>
2020-10-04 21:45:20 +0500

    Update renew script

--- ssl-renew.sh
+++ ssl-renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f PATH_TO_PROJECT_DIR/docker-compose.yml run certbot renew --dry-run \
-&& /usr/local/bin/docker-compose -f PATH_TO_PROJECT_DIR/docker-compose.yml kill -s SIGHUP nginx
+docker-compose -f PATH_TO_PROJECT_DIR/docker-compose.yml run certbot renew --dry-run \
+&& docker exec nginx nginx -s reload
